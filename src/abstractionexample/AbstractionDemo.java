e038b6bb59541978cab59b00884d2d23daa03812
sohal <sohalchunarkar8@gmail.com>
2025-12-16 05:50:21 +0530

    i have added demo 33

--- src/abstractionexample/AbstractionDemo.java
+++ src/abstractionexample/AbstractionDemo.java
@@ -10,6 +10,10 @@ public abstract class AbstractionDemo {
         System.out.println("demo2 concrete method inside AbstractionDemo class");
     }
 
+    public void demo33(){
+        System.out.println("hellow this isnew change");
+    }
+
     public abstract void demo3();
 
     public abstract void demo4();
